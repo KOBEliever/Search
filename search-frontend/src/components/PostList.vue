2720503ddcaa0b4d2df1b92742016ead070dbbc9
DingYi <970102437@qq.com>
2023-10-14 15:47:17 +0800

    页面初步完成， 使用url记录页面搜索状态，用户刷新界面时可以从url还原搜索状态 1.用户操作时改变url（搜索时将搜索内容填充、分页信息） 2.url改变时，改变页面状态（使用route监听url）

---
+++ search-frontend/src/components/PostList.vue
@@ -0,0 +1,10 @@
+<template>
+帖子列表
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
---
+++ search-frontend/src/pages/IndexPages.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="index_page">
+    <a-input-search
+        v-model:value="searchParam.text"
+        placeholder="input search text"
+        enter-button="Search"
+        size="large"
+        @search="onSearch"
+    />
+
+    <MyDivider />
+
+    <a-tabs v-model:activeKey="activeKey" @change="onTabChange">
+      <a-tab-pane key="post" tab="Post">
+        <PostList />
+      </a-tab-pane>
+      <a-tab-pane key="picture" tab="Picture">
+        <PictureList />
+      </a-tab-pane>
+      <a-tab-pane key="user" tab="User">
+        <UserList />
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {ref, watchEffect} from "vue";
+import PostList from "@/components/PostList.vue";
+import PictureList from "@/components/PictureList.vue";
+import UserList from "@/components/UserList.vue";
+import MyDivider from "@/components/MyDivider.vue";
+import {useRoute, useRouter} from "vue-router";
+
+const router = useRouter()
+const route = useRoute();
+const activeKey = route.params.category;
+const initSearchParams = {
+  text : "",
+  pageSize: 10,
+  pageNum: 1,
+};
+const searchParam = ref(initSearchParams)
+
+watchEffect(() => {
+  searchParam.value = {
+    ...initSearchParams,
+    text: route.query.text,
+  }as any
+})
+ const onSearch = (value : string) => {
+   alert(value);
+   router.push({
+     query : searchParam.value
+   })
+ };
+
+const onTabChange = (key : string) => {
+  router.push({
+    path : `/${key}`,
+    query : searchParam.value
+  })
+}
+</script>
