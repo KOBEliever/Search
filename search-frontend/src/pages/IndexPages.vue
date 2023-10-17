9cf78d112e0f20a8bb63f86dcf5dc957a5a311c2
DingYi <970102437@qq.com>
2023-10-17 17:10:03 +0800

    改为根据tab和搜索内容触发对应内容的搜索，调用后端门面搜索模式接口

--- search-frontend/src/pages/IndexPages.vue
+++ search-frontend/src/pages/IndexPages.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="index_page">
+  <div class="index-page">
     <a-input-search
-        v-model:value="searchParam.text"
+        v-model:value="searchText"
         placeholder="input search text"
         enter-button="Search"
         size="large"
@@ -9,13 +9,13 @@
     />
     <MyDivider />
     <a-tabs v-model:activeKey="activeKey" @change="onTabChange">
-      <a-tab-pane key="post" tab="Post">
-        <PostList :post-list="postList"/>
+      <a-tab-pane key="post" tab="文章">
+        <PostList :post-list="postList" />
       </a-tab-pane>
-      <a-tab-pane key="picture" tab="Picture">
+      <a-tab-pane key="picture" tab="图片">
         <PictureList :picture-list="pictureList" />
       </a-tab-pane>
-      <a-tab-pane key="user" tab="User">
+      <a-tab-pane key="user" tab="用户">
         <UserList />
       </a-tab-pane>
     </a-tabs>
@@ -23,74 +23,116 @@
 </template>
 
 <script setup lang="ts">
-
-import {ref, watchEffect} from "vue";
+import { ref, watchEffect } from "vue";
 import PostList from "@/components/PostList.vue";
 import PictureList from "@/components/PictureList.vue";
 import UserList from "@/components/UserList.vue";
 import MyDivider from "@/components/MyDivider.vue";
-import {useRoute, useRouter} from "vue-router";
-import myAxios from "@/plugins/MyAxios";
+import { useRoute, useRouter } from "vue-router";
+import myAxios from "../plugins/MyAxios";
+import { message } from "ant-design-vue";
 
-const router = useRouter();
+const postList = ref([]);
+
+const pictureList = ref([]);
+
 const route = useRoute();
+const router = useRouter();
 const activeKey = route.params.category;
+
 const initSearchParams = {
-  text : "",
+  type: activeKey,
+  text: "",
   pageSize: 10,
   pageNum: 1,
 };
-const postList = ref([]);
-const pictureList = ref([]);
-const searchParam = ref(initSearchParams);
+
+const searchText = ref(route.query.text || "");
+
 /**
- *
+ * 加载数据
+ * @param params
  */
-const loadDataOld = ((params:any) => {
+const loadDataOld = (params: any) => {
   const postQuery = {
     ...params,
     searchText: params.text,
   };
-  myAxios.post('post/list/page/vo',postQuery).then((res:any) => {
+  myAxios.post("post/list/page/vo", postQuery).then((res: any) => {
     postList.value = res.records;
-  })
+  });
+
   const pictureQuery = {
     ...params,
     searchText: params.text,
   };
-  myAxios.post('picture/list/page/vo',pictureQuery).then((res:any) => {
+  myAxios.post("picture/list/page/vo", pictureQuery).then((res: any) => {
     pictureList.value = res.records;
-  })
-})
-const loadData = ((params:any) => {
+  });
+};
+
+/**
+ * 加载聚合数据
+ * @param params
+ */
+const loadAllData = (params: any) => {
   const query = {
     ...params,
     searchText: params.text,
   };
-  myAxios.post('search/all',query).then((res:any) => {
+  myAxios.post("search/all", query).then((res: any) => {
     postList.value = res.postList;
     pictureList.value = res.pictureList;
-  })
-})
+  });
+};
+
+/**
+ * 加载单类数据
+ * @param params
+ */
+const loadData = (params: any) => {
+  const { type } = params;
+  if (!type) {
+    message.error("类别为空");
+    return;
+  }
+  const query = {
+    ...params,
+    searchText: params.text,
+  };
+  myAxios.post("search/all", query).then((res: any) => {
+    if (type === "post") {
+      postList.value = res.postList;
+    } else if (type === "picture") {
+      pictureList.value = res.pictureList;
+    }
+  });
+};
+
+const searchParams = ref(initSearchParams);
+
 watchEffect(() => {
-  searchParam.value = {
+  searchParams.value = {
     ...initSearchParams,
     text: route.query.text,
-  }as any
+    type: route.params.category,
+  } as any;
+  loadData(searchParams.value);
 });
 
-loadData(initSearchParams);
- const onSearch = (value : string) => {
-   router.push({
-     query : searchParam.value
-   });
-   loadData(searchParam.value);
- };
-
-const onTabChange = (key : string) => {
+const onSearch = (value: string) => {
   router.push({
-    path : `/${key}`,
-    query : searchParam.value,
-  })
-}
+    query: {
+      ...searchParams.value,
+      text: value,
+    },
+  });
+};
+
+const onTabChange = (key: string) => {
+  router.push({
+    path: `/${key}`,
+    query: searchParams.value,
+  });
+};
 </script>
